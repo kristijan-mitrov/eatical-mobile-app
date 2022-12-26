64c3d238344d674d972145cd72c8ed445f36654e
makedonkabinova <makedonka.binova@student.um.si>
2022-12-26 13:44:53 +0100

    Open Street Maps

--- app/src/main/java/com/example/eatical_mobile_app/MainActivity.kt
+++ app/src/main/java/com/example/eatical_mobile_app/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.eatical_mobile_app
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +17,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.eatical_mobile_app.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import org.osmdroid.util.GeoPoint
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,6 +25,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var longitude: Double = 0.0
     private var latitude: Double = 0.0
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == Activity.RESULT_OK){
+            val data = it?.data
+            if(data != null) {
+                val coordinates = data.getParcelableExtra<GeoPoint>("coordinates")
+                if (coordinates != null) {
+                    longitude = coordinates.longitude
+                    latitude = coordinates.latitude
+                    Toast.makeText(
+                        this,
+                        "New Location:(" + longitude.toString() + ", " + latitude.toString() + ")",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else throw Exception(R.string.coordinates_null.toString())
+            }else throw Exception(R.string.data_null.toString())
+        }else {
+            println(R.string.user_did_not_choose_coordinates)
+            Toast.makeText(this, R.string.device_location_set, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     private val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
@@ -53,7 +76,7 @@ class MainActivity : AppCompatActivity() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-            openGalley()
+            openGallery()
         } else {
             showDialogue(R.string.gallery_permission_not_granted_error)
         }
@@ -82,7 +105,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Camera", Toast.LENGTH_SHORT).show()
     }
 
-    private fun openGalley(){
+    private fun openGallery(){
         Toast.makeText(this, "Gallery", Toast.LENGTH_SHORT).show()
     }
 
@@ -118,6 +141,10 @@ class MainActivity : AppCompatActivity() {
         intervalShooterButton.setOnClickListener {
 //            if(optionIsSelected()) // TODO: Shoot Photos on interval
         }
+
+        mapButton.setOnClickListener{
+            viewModel.setState(MainStates.CHOOSE_LOCATION)
+        }
     }
 
     private fun observeCheckboxes() = with(binding){
@@ -145,6 +172,7 @@ class MainActivity : AppCompatActivity() {
                 MainStates.GETTING_LOCATION_PERMISSION -> getLocationPermission()
                 MainStates.GETTING_CAMERA_PERMISSION -> getCameraPermission()
                 MainStates.GETTING_GALLERY_PERMISSION -> getGalleryPermission()
+                MainStates.CHOOSE_LOCATION -> getMap()
             }
         }
     }
@@ -163,6 +191,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun getGalleryPermission(){
         galleryPermissionRequest.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+    private fun getMap(){
+        launcher.launch(Intent(this, MapActivity::class.java))
     }
 
     private fun showDialogue(@StringRes resId: Int){
---
+++ app/src/main/java/com/example/eatical_mobile_app/MyMapEventsReceiver.kt
@@ -0,0 +1,36 @@
+package com.example.eatical_mobile_app
+
+import android.content.Context
+import org.osmdroid.events.MapEventsReceiver
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+
+class MyMapEventsReceiver(var map: MapView, var context: Context) : MapEventsReceiver {
+    var point: GeoPoint? = null
+    var previousMarker: Marker? = null
+    var marker: Marker? = null
+    override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
+        point = p
+        marker = Marker(map)
+        if (p != null) {
+            marker!!.position.setCoords(
+                p.latitude,
+                p.longitude
+            )
+            if (previousMarker != null)
+                map.overlays.remove(previousMarker)
+            previousMarker = marker
+        }
+        marker!!.icon = context.getResources().getDrawable(R.drawable.map_pin, null)
+        marker!!.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        map.overlays.add(marker)
+        map.invalidate()
+        return true
+    }
+
+    override fun longPressHelper(p: GeoPoint?): Boolean {
+        println("Long press on " + p.toString())
+        return true
+    }
+}
